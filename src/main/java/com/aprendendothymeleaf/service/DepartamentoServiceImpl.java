b16dd68d794b8f32d89f6588ac712bff8c5ca66b
duhhdm <duhhdm@hotmail.com>
2019-07-07 00:07:41 -0300

    Quarto Dia 1.0

---
+++ src/main/java/com/aprendendothymeleaf/service/DepartamentoServiceImpl.java
@@ -0,0 +1,51 @@
+package com.aprendendothymeleaf.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aprendendothymeleaf.dao.DepartamentoDAO;
+import com.aprendendothymeleaf.domain.Departamento;
+
+@Service
+@Transactional(readOnly=false)
+public class DepartamentoServiceImpl implements DepartamentoService{
+
+	@Autowired
+	private DepartamentoDAO departamentoDao;
+	
+	@Override
+	public void salvar(Departamento departamento) {
+		// TODO Auto-generated method stub
+		departamentoDao.save(departamento);
+	}
+
+	@Override
+	public void atualizar(Departamento departamento) {
+		// TODO Auto-generated method stub
+		departamentoDao.update(departamento);
+	}
+
+	@Override
+	public void deletar(Integer id) {
+		// TODO Auto-generated method stub
+		departamentoDao.delete(id);
+	}
+
+	@Transactional(readOnly=true)
+	@Override
+	public Departamento buscarId(Integer id) {
+		// TODO Auto-generated method stub
+		return departamentoDao.findById(id);
+	}
+
+	@Transactional(readOnly=true)
+	@Override
+	public List<Departamento> bustarTodos() {
+		// TODO Auto-generated method stub
+		return departamentoDao.findAll();
+	}
+	
+}
---
+++ src/main/java/com/aprendendothymeleaf/service/FuncionarioServiceImpl.java
@@ -0,0 +1,50 @@
+package com.aprendendothymeleaf.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aprendendothymeleaf.dao.FuncionarioDAO;
+import com.aprendendothymeleaf.domain.Funcionario;
+@Service
+@Transactional(readOnly=false)
+public class FuncionarioServiceImpl implements FuncionarioService{
+
+	@Autowired
+	private FuncionarioDAO funcionarioDao;
+	
+	@Override
+	public void salvar(Funcionario funcionario) {
+		// TODO Auto-generated method stub
+		funcionarioDao.save(funcionario);
+	}
+
+	@Override
+	public void atualizar(Funcionario funcionario) {
+		// TODO Auto-generated method stub
+		funcionarioDao.update(funcionario);
+	}
+
+	@Override
+	public void deletar(Integer id) {
+		// TODO Auto-generated method stub
+		funcionarioDao.delete(id);
+	}
+	
+	@Transactional(readOnly=true)
+	@Override
+	public Funcionario buscarId(Integer id) {
+		// TODO Auto-generated method stub
+		return funcionarioDao.findById(id);
+	}
+
+	@Transactional(readOnly=true)
+	@Override
+	public List<Funcionario> bustarTodos() {
+		// TODO Auto-generated method stub
+		return funcionarioDao.findAll();
+	}
+	
+}
