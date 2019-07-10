e74b462ca14bd3bb585ccc059125cd95ff1ea36a
duhhdm <duhhdm@hotmail.com>
2019-07-09 21:42:43 -0300

    Quinto Dia 1.2

--- src/main/java/com/aprendendothymeleaf/controller/FuncionarioController.java
+++ src/main/java/com/aprendendothymeleaf/controller/FuncionarioController.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.aprendendothymeleaf.domain.Cargo;
 import com.aprendendothymeleaf.domain.Funcionario;
@@ -32,6 +34,18 @@ public class FuncionarioController {
 		return "/funcionario/cadastro";
 	}
 	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Integer id, ModelMap model) {
+		model.addAttribute("funcionario", funcServico.buscarId(id));
+		return "/funcionario/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Funcionario funcionario, RedirectAttributes obj) {
+		funcServico.atualizar(funcionario);
+		return"redirect:/funcionarios/listar";
+	}
+	
 	@PostMapping("/salvar")
 	public String salvar(Funcionario funcionario) {
 		funcServico.salvar(funcionario);
@@ -42,6 +56,13 @@ public class FuncionarioController {
 	public String listar(ModelMap model) {
 		model.addAttribute("funcionario", funcServico.bustarTodos());
 		return "/funcionario/lista";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable Integer id, ModelMap model) {
+		model.addAttribute("success", "o funcionario foi excluido com sucesso");
+		funcServico.deletar(id);
+		return "redirect:/funcionarios/listar";
 	}
 	
 	@ModelAttribute("cargos")
